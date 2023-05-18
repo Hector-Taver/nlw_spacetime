928c5281e82eba814062484b5a70b32060f41784
Hector van Tol Taver <hectorvttaver@gmail.com>
2023-05-18 17:40:42 -0300

    Initial commit

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,21 @@
+import { Inter } from 'next/font/google'
+import { ReactNode } from 'react'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Cápsula do tempo',
+  description: 'Cápsula do tempo para guardar memórias passadas numa timeline.',
+  icons: {
+    icon: '/favicon.png',
+  },
+}
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
