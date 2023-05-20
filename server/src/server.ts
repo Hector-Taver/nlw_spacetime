231e2157669ef2ca4a9ef78ee6382af6f96c95c8
Hector van Tol Taver <hectorvttaver@gmail.com>
2023-05-20 01:39:09 -0300

    Created JWT

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import fastify from 'fastify'
 
 import { authRoutes } from './routes/auth'
@@ -10,6 +11,10 @@ const app = fastify()
 
 app.register(cors, {
   origin: true,
+})
+
+app.register(jwt, {
+  secret: 'spacetime',
 })
 
 app.register(authRoutes)
